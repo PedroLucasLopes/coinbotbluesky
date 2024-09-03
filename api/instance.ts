9940c2e84ddabcb5db77de1d650008fb5561a7e2
PedroLucasLopes <pedrolopesparaguai@gmail.com>
2024-09-03 10:02:02 -0300

    Fix files

--- api/instance.ts
+++ api/instance.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: "https://api.coingecko.com/api/v3",
   timeout: 1000,
   headers: {
-    x_cg_api_key: "CG-DfyS63LUJe4oubdRqX8hv87K",
+    x_cg_api_key: process.env.COINGECKO_KEY!,
     "Content-Type": "application/json",
   },
   withCredentials: false,
--- bot/session.ts
+++ bot/session.ts
@@ -1,10 +1,7 @@
 import { AtpAgent } from "@atproto/api";
 import instance from "../api/instance";
-import { json } from "stream/consumers";
-require("dotenv").config();
 
 const startSession = async () => {
-  const url = "https://api.coingecko.com/api/v3/";
   const agent = new AtpAgent({
     service: "https://bsky.social/",
   });
